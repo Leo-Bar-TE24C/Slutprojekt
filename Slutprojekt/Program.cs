4f6215f75e7b50e4ca8497531c3834e2682bdcec
Leo-Bar-TE24C <leonardo.barajasrios@elev.ga.ntig.se>
2026-03-03 11:09:31 +0100

    starting pseudo code

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -5,10 +5,20 @@ Raylib.SetTargetFPS(60);
 
 while(Raylib.WindowShouldClose() == false)
 {
+
     
+    // if to the sides dodge (take no damage)
+    // if back block (take less damage)
+    // if spacebar hit punch
+    // if up and spacebar high punch
+    // if punch and no block or dodge deal damage
+
+
+
+
     Raylib.BeginDrawing();
 
-
+    // play animations based on combat
 
     Raylib.EndDrawing();
 }
--- Slutprojekt/Toolbox.cs
+++ Slutprojekt/Toolbox.cs
@@ -1,4 +1,20 @@
 public class Toolbox
 {
-    
+    void Combat()
+    {
+        // player
+        {
+             // if to the sides dodge (take no damage)
+    // if back block (take less damage)
+    // if spacebar hit punch
+    // if up and spacebar high punch
+    // if punch and no block or dodge deal damage
+        }
+
+        // enemy
+        {
+            // pick random action from moveset 
+            // stun if hit
+        }
+    }
 }
